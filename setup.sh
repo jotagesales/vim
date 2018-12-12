2637481162439cca670caac88627199f72ff1333
Jotagê Sales <jose.sales@fs.com.br>
2018-12-12 19:26:48 -0200

    tests

--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo 'TESTE do jotinha remote'
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
